a79f86f7489fa7b26e646049aadd7b4751944d55
Magic8Ball <seanwolf90@gmail.com>
2026-09-19 14:06:05 -0500

    Forward modern spread and strengthen recoil

--- regamedll/dlls/wpn_shared/wpn_deagle.cpp
+++ regamedll/dlls/wpn_shared/wpn_deagle.cpp
@@ -165,6 +165,7 @@ void CDEAGLE::DEAGLEFire(float flSpread, float flCycleTime, BOOL fUseSemi)
 	const gw::ShotOffset offset = gw::ComputeShotOffset(m_pPlayer->random_seed, flSpread, config.baseSpread);
 	vecAiming = vecAiming + gpGlobals->v_right * offset.x + gpGlobals->v_up * offset.y;
 	vecDir = m_pPlayer->FireBullets3(vecSrc, vecAiming, 0.0f, 4096, 2, BULLET_PLAYER_50AE, flBaseDamage, DEAGLE_RANGE_MODIFER, m_pPlayer->pev, true, m_pPlayer->random_seed);
+	vecDir = Vector(offset.x, offset.y, 0.0f);
 
 #ifdef CLIENT_WEAPONS
 	flag = FEV_NOTHOST;
